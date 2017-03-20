ca15e1eac9cae4fbcdb8d174022a424cb6a44ea9
Raul <xraul94@gmail.com>
2017-03-20 12:25:37 +0000

    Update SampleController.java

--- src/main/java/com/Galeno/controlador/SampleController.java
+++ src/main/java/com/Galeno/controlador/SampleController.java
@@ -12,7 +12,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Galeno-Historial Clinico";
     }
 
     public static void main(String[] args) throws Exception {
